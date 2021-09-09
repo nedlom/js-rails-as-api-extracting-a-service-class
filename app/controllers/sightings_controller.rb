d0335032e4ac61ce4bd41168dfffeb8fdf0b1fe0
Nick <nelom3@gmail.com>
2021-09-09 11:25:24 -0700

    commit tree ed1258798698f20454c22056dfc632cd101637b1

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,6 +1,11 @@
 class SightingsController < ApplicationController
-    def show
-        @sighting = Sighting.find(params[:id])
-        render json: @sighting.to_json(:include => {:bird => {:only =>[:name, :species]}, :location => {:only =>[:latitude, :longitude]}}, :except => [:updated_at])
-    end
+  def index
+    sighting = Sighting.all
+    render json: Sighting.new(sighting).to_serialized_json
+  end
+  
+  def show
+    sighting = Sighting.find(params[:id])
+    render json: Sighting.new(sighting).to_serialized_json
+  end
 end
